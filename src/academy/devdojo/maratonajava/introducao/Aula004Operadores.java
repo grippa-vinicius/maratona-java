d088d23b489e0d62626546c32b6fdf4f6f3e2fce
grippa-vinicius <viniciusdegoesgrippa@gmail.com>
2021-09-16 05:30:31 -0300

    Operadores Aritméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula004Operadores.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula004Operadores {
+    public static void main(String[] args) {
+
+        /*
+            Operadores Aritméticos:
+            Adição: +
+            Subtração: -
+            Multiplicação: *
+            Divisão: /
+         */
+
+        int numero1 = 10;
+        int numero2 = 20;
+        double resultado = numero1 + numero2;
+
+        System.out.println(resultado);
+
+
+    }
+}
